06d034e60f437aed8c4729dc6dc03129e077a100
ke_ix1 <xkei4dev@gmail.com>
2018-04-25 23:06:34 +0900

    add SensorAdapter

--- app/src/main/java/com/keix1/btcon/MainActivity.java
+++ app/src/main/java/com/keix1/btcon/MainActivity.java
@@ -3,8 +3,11 @@ package com.keix1.btcon;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.UUID;
+import com.keix1.btcon.SensorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     private String MacAddress = "DC:A9:04:84:9D:1B";
     private String MY_UUID = "00001101-0000-1000-8000-00805F9B34FB";
 //    private String MY_UUID = "21001101-0000-1000-8000-00805F9B34FB";
+    private SensorAdapter sensorAdapter;
+    private float[] position = {0.0f, 0.0f, 0.0f};
+    private int[] positionInt = {0, 0, 0};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +46,9 @@ public class MainActivity extends AppCompatActivity {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                setZero();
                 if(mBTSocket != null) {
-                    Send();
+//                    SendZero();
                 }
             }
         });
@@ -62,6 +71,46 @@ public class MainActivity extends AppCompatActivity {
             btnSend.setText("mBTSocket == null !!");
         }
 
+
+        SensorManager mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        sensorAdapter = new SensorAdapter(mSensorManager);
+
+
+//        new Thread(new Runnable(){
+//            @Override
+//            public void run() {
+//                while(true) {
+//                    try {
+//                        Thread.sleep(100);
+////                        position[0] += sensorAdapter.getx() * 10;
+////                        position[1] += sensorAdapter.gety() * 10;
+////                        position[2] += sensorAdapter.getz() * 10;
+                        positionInt[0] += (int)sensorAdapter.getx() * 10;
+                        positionInt[1] += (int)sensorAdapter.gety() * 10;
+                        positionInt[2] += (int)sensorAdapter.getz() * 10;
+//                    } catch(Exception e) {
+//
+//                    }
+//                }
+//
+//            }
+//        }).start();
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Thread.sleep(100);
+                        Send();
+                    } catch(Exception e) {
+
+                    }
+                }
+
+            }
+        }).start();
 
     }
 
@@ -98,7 +147,54 @@ public class MainActivity extends AppCompatActivity {
     private void Send(){
         //文字列を送信する
         byte[] bytes = {};
-        String str = "Hello World!";
+
+
+        position[0] += sensorAdapter.getx();
+        position[1] += sensorAdapter.gety();
+        position[2] += sensorAdapter.getz();
+//        positionInt[0] += (int)sensorAdapter.getx() * 30;
+//        positionInt[1] += (int)sensorAdapter.gety() * 30;
+//        positionInt[2] += (int)sensorAdapter.getz() * 30;
+
+        String x = String.format("%.1f", position[0]);
+        String y = String.format("%.1f", position[1]);
+        String z = String.format("%.1f", position[2]);
+//        String x = "" + position[0];
+//        String y = "" + position[1];
+//        String z = "" + position[2];
+
+        String str = "{" + x + "," + y + "," + z + "}";
+        Log.d("TAGGGGGG", str);
+        bytes = str.getBytes();
+        try {
+            //ここで送信
+            mOutputStream.write(bytes);
+        } catch (IOException e) {
+            try{
+                mBTSocket.close();
+            }catch(IOException e1){/*ignore*/}
+        }
+    }
+
+    private void setZero() {
+        positionInt[0] = 0;
+        positionInt[1] = 0;
+        positionInt[2] = 0;
+
+        position[0] = 0.0f;
+        position[1] = 0.0f;
+        position[2] = 0.0f;
+    }
+
+    private void SendZero(){
+        //文字列を送信する
+        byte[] bytes = {};
+        String x = "0";
+        String y = "0";
+        String z = "0";
+
+        String str = "{" + x + "," + y + "," + z + "}";
+        Log.d("TAGGGGGG", str);
         bytes = str.getBytes();
         try {
             //ここで送信
@@ -120,4 +216,5 @@ public class MainActivity extends AppCompatActivity {
             mBTSocket = null;
         }
     }
+
 }
